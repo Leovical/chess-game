474754bc223838350e1905ace06658551a5f7549
Leonardo Victor <leovictor.alves@hotmail.com>
2025-09-20 19:51:43 -0300

    adding methods validateSourcePosition, makeMove and performChessMove on ChessMatch. ChessException now extends BoardException. Testing the adjustments on Program

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,31 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		
-		Interface.printBoard(chessMatch.getPieces());
+		//testing
+		while (true) {
+			Interface.printBoard(chessMatch.getPieces());
+			System.out.println();
+			System.out.print("Source: ");
+			ChessPosition source = Interface.readChessPosition(sc);
+			
+			System.out.println();
+			
+			System.out.print("Target: ");
+			ChessPosition target = Interface.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		}
 	}
-
 }
