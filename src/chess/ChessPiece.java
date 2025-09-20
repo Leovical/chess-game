15b3ac0d031a5398a2e5cad0e0da4d630c712676
Leonardo Victor <leovictor.alves@hotmail.com>
2025-09-20 20:47:06 -0300

    on ChessPiece, method isThereOpponentPiece added

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -2,6 +2,7 @@ package chess;
 
 import gameboard.Board;
 import gameboard.Piece;
+import gameboard.Position;
 
 public abstract class ChessPiece extends Piece {
 
@@ -14,6 +15,11 @@ public abstract class ChessPiece extends Piece {
 
 	public Color getColor() {
 		return color;
+	}
+	
+	protected boolean isThereOpponentPiece(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p != null && p.getColor() != color;
 	}
 	
 }
