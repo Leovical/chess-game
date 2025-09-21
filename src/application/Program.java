95b5c7d2d05aac0fa14adc3492e74fb965414a4a
Leonardo Victor <leovictor.alves@hotmail.com>
2025-09-20 21:41:27 -0300

    painting piece's possible moves

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,6 +24,11 @@ public class Program {
 				System.out.print("Source: ");
 				ChessPosition source = Interface.readChessPosition(sc);
 				
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				Interface.clearScreen();
+				
+				Interface.printBoard(chessMatch.getPieces(), possibleMoves);
+				
 				System.out.println();
 				
 				System.out.print("Target: ");
